Fix incorrect indexing for img_std in input normalization
img_std was using the same slice [:3] as img_mean. It should use [3:]
to correctly reference the std values in self.img_mean_std.

Fixes #3

task/core.py
@@ -75,7 +75,7 @@ class CrossScoreNet(torch.nn.Module):
 
         if norm_img:
             img_mean = self.img_mean_std[None, :3, None, None]
-            img_std = self.img_mean_std[None, :3, None, None]
+            img_std = self.img_mean_std[None, 3:, None, None]
             query_img = (query_img - img_mean) / img_std
             if ref_cross_imgs is not None:
                 ref_cross_imgs = (ref_cross_imgs - img_mean[:, None]) / img_std[:, None]